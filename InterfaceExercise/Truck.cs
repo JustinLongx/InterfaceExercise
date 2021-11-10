75046463a1affdccd169f6caea83a47f193e8c01
justin long <justinxlong93@gmail.com>
2021-11-10 15:21:27 -0600

    Initial commit

---
+++ InterfaceExercise/Truck.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceExercise
+{
+    public class Truck : IVehicle, ICompany
+    {
+
+        public double EngineSize { get; set; } = 6;
+        public string Model { get; set; } = "Ford";
+        public string Make { get; set; } = "F150";
+        public int SeatCount { get; set; } = 4;
+        public string CompanyName { get; set; } = "Ford";
+        public string Motto { get; set; } = "Built Ford tough";
+        public bool HasChangedGears { get; set; }
+
+        public bool HasFourWheelDrive { get; set; }
+
+        public void Drive()
+        {
+            if (HasFourWheelDrive == true)
+            {
+                Console.WriteLine($"4 wheel drive {GetType().Name} now driving forward..");
+            }
+            else
+            {
+                Console.WriteLine($"{GetType().Name} now driving forward..");
+            }
+        }
+
+        
+        public void Reverse()
+        {
+            if (HasChangedGears == true)
+            {
+                Console.WriteLine($"{GetType().Name} now reversing...");
+                HasChangedGears = false;
+            }
+            else
+            {
+                Console.WriteLine($"Cant reverse until we change gears.");
+            }
+        }
+        public void Park()
+        {
+            if (HasChangedGears == true)
+            {
+                Console.WriteLine($"{GetType().Name} now reversing...");
+                HasChangedGears = false;
+            }
+            else
+            {
+                Console.WriteLine($"Cant park until we change gears.");
+            }
+        }
+        public void ChangeGears(bool isChanged)
+        {
+            HasChangedGears = isChanged;
+        }
+    }
+
+
+}
